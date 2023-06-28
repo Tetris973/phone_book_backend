c61b6af299c7e937e14ec828325d7d789b8c64c2
Tetris <cedric.olender@gmail.com>
2023-06-28 16:00:04 +0200

    exercice 3.19 - 3.21

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
   next(error)
 }
@@ -64,7 +66,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch((error) => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
@@ -77,7 +79,6 @@ app.post('/api/persons', (request, response) => {
       error: 'number missing',
     })
   }
-
   Person.find({ name: body.name }).then((persons) => {
     if (persons.length > 0) {
       return response.status(400).json({
@@ -91,9 +92,12 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson)
-  })
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -101,7 +105,12 @@ app.put('/api/persons/:id', (request, response, next) => {
   const person = {
     number: body.number,
   }
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
     .then((updatedPerson) => {
       response.json(updatedPerson)
     })
--- models/person.js
+++ models/person.js
@@ -15,8 +15,22 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: [true, 'User phone number required'],
+    validate: {
+      validator: function (v) {
+        return /^(0\d{1,2}-\d{7,})$/.test(v)
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! A phone number must have a length of 8 or more and be formed of two parts that are separated by -, the first part has two or three numbers and the second part also consists of numbers. Eg. 09-1234556 and 040-22334455 are valid phone numbers.`,
+    },
+  },
 })
 
 personSchema.set('toJSON', {
